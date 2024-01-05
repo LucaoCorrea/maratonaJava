bed9398d16e2956e410da4b526f199091f0878c0
LucaoCorrea <lucascorrea1505@gmail.com>
2024-01-04 23:14:52 -0300

    Finalizando Operadores

--- src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
+++ src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -4,7 +4,7 @@ public class Aula04Operadores {
     public static void main(String[] args) {
         int numero1 = 25;
         double numero2 = 12;
-        double resultado = (short) (numero1 /numero2); //casting
+        double resultado = (short) (numero1 / numero2); //casting
         System.out.println(resultado);
 
         //% - podemos ver se o number é par ou impar
@@ -23,5 +23,35 @@ public class Aula04Operadores {
         System.out.println("isDezMenorQueVinte " + isDezMenorQueVinte);
         System.out.println("isDezIgualQueVinte " + isDezIgualQueVinte);
 
+        // AND - &&
+        // OR - ||
+        // NO - !
+
+        int idade = 35;
+        float salario = 3500F;
+        boolean isDentroDaLeiMaiorQueTrinta = idade > 30 && salario >= 4612;
+        boolean isDentroDaLeiMenorQueTrinta = idade < 30 && salario >= 3381;
+
+        System.out.println(isDentroDaLeiMaiorQueTrinta);
+        System.out.println(isDentroDaLeiMenorQueTrinta);
+
+        double valorTotalContaCorrente = 200;
+        double valorTotalContaPoupanca = 10000;
+        float valorPlaystation = 5000F;
+
+        boolean isPlaystationCincoCompravel = valorTotalContaCorrente > valorPlaystation || valorTotalContaPoupanca > valorPlaystation ;
+        System.out.println(isPlaystationCincoCompravel);
+
+        // = += -= *= /= %=
+        double bonus = 1800;
+        bonus += 1000;
+        System.out.println("O Bônus é de " + bonus);
+
+        // ++ --
+        int contador = 0;
+        contador += 1; // contador = contador + 1
+        contador++; // contador = contador + 1
+        contador--; // contador = contador - 1
+        System.out.println(contador);
     }
 }
