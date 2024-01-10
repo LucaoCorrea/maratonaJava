948a4c1496b500ced08cc7e50869705b66c028c6
LucaoCorrea <lucascorrea1505@gmail.com>
2024-01-10 00:06:29 -0300

    Orientação Objetos Classes

---
+++ src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/test/CarroTest.java
@@ -0,0 +1,20 @@
+package academy.devdojo.maratonajava.javacore.Aintroducaoclasses.test;
+
+import academy.devdojo.maratonajava.javacore.Aintroducaoclasses.domain.Carro;
+
+public class CarroTest {
+    public static void main(String[] args) {
+        Carro carro1 = new Carro();
+        Carro carro2 = new Carro();
+        carro1.nome = "Honda Civic";
+        carro1.modelo = "86";
+        carro1.ano = 1986;
+
+        carro2.nome = "Ford Ka";
+        carro2.modelo = "Sedan";
+        carro2.ano = 2016;
+
+        System.out.println(carro1.nome + " " + carro1.modelo + " " + carro1.ano);
+        System.out.println(carro2.nome + " " + carro2.modelo + " " + carro2.ano);
+    }
+}
---
+++ src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/test/EstudanteTest01.java
@@ -0,0 +1,18 @@
+package academy.devdojo.maratonajava.javacore.Aintroducaoclasses.test;
+
+import academy.devdojo.maratonajava.javacore.Aintroducaoclasses.domain.Estudante;
+
+public class EstudanteTest01 {
+    public static void main(String[] args) {
+        Estudante estudante = new Estudante();
+        estudante.nome  = "Lucas Corrêa";
+        estudante.idade  = 18;
+        estudante.sexo = 'M';
+
+        System.out.println(estudante.nome);
+        System.out.println(estudante.idade);
+        System.out.println(estudante.sexo);
+        System.out.println(estudante);
+
+    }
+}
